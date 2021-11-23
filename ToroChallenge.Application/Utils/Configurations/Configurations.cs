fd292c60959bc66af182f21bfce093c163737f8f
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-23 14:59:01 -0300

    Adiciona utilitário de configurações e constantes

---
+++ ToroChallenge.Application/Utils/Configurations/Configurations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToroChallenge.Application.Utils.Configurations
+{
+    public static class Configurations
+    {
+        public static string GetDefaultConnectionString { get; } = "Server=192.168.0.114,1433;Database=investimentos;UID=sa;PWD=Pwdsafe@123;";
+    }
+}
---
+++ ToroChallenge.Application/Utils/Constantes/TabelasConst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToroChallenge.Application.Utils.Constantes
+{
+    public static class TabelasConst
+    {
+        public const string Ativos = "tb_ativos";
+        public const string Patrimonios = "tb_patrimonios";
+        public const string PatrimonioAtivos = "tb_patrimonio_ativos";
+        public const string Usuarios = "tb_usuarios";
+    }
+}
