f0529071236b9460171e94d102895cf1e2a59a4d
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-23 15:00:36 -0300

    Adiciona parametro de configuração a injeção de dependência

--- ToroChallenge.Infrastructure/DependencyInjection.cs
+++ ToroChallenge.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace ToroChallenge.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection RegisterInfrastructureServices(this IServiceCollection services)
+        public static IServiceCollection RegisterInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             return services;
         }
