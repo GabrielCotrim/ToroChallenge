8fe21020b254a54efb890b6a9ae681cfe0162bb1
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-23 14:59:57 -0300

    Aplica configurações de mapping às entidades

---
+++ ToroChallenge.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToroChallenge.Application.Utils.Configurations;
+using ToroChallenge.Domain.Entities;
+using ToroChallenge.Infrastructure.Mapping;
+
+namespace ToroChallenge.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Ativo> Ativos { get; set; }
+        public DbSet<Patrimonio> Patrimonios { get; set; }
+        public DbSet<PatrimonioAtivos> PatrimonioAtivos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) optionsBuilder.UseSqlServer(Configurations.GetDefaultConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new AtivoMapping());
+            modelBuilder.ApplyConfiguration(new UsuarioMapping());
+            modelBuilder.ApplyConfiguration(new PatrimonioMapping());
+            modelBuilder.ApplyConfiguration(new PatrimonioAtivosMapping());
+        }
+    }
+}
