e1e80b58b9a2c37239f77f008e517bca9c43baae
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-24 11:31:53 -0300

    Adiciona interface de repositório

---
+++ ToroChallenge.Application/Interfaces/Repositories/IPatrimonioRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Domain.Entities;
+
+namespace ToroChallenge.Application.Interfaces.Repositories
+{
+    public interface IPatrimonioRepository
+    {
+        public Task<Patrimonio> ObtenhaPatrimonioDoUsuario(int idUser);
+    }
+}
