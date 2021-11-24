88a993d46c95e960282564ba56049fac4936caf9
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-24 11:32:47 -0300

    Adiciona mapping

---
+++ ToroChallenge.Application/AutoMapper/AutoMapperDependencyinjection.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using ToroChallenge.Application.AutoMapper.Mappings;
+
+namespace ToroChallenge.Application.AutoMapper
+{
+    public static class AutoMapperDependencyinjection
+    {
+        public static IServiceCollection RegisterAutoMapper(this IServiceCollection services)
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            var mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+            return services;
+        }
+    }
+}
---
+++ ToroChallenge.Application/AutoMapper/Mappings/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Application.DTOs.Responses;
+using ToroChallenge.Domain.Models;
+
+namespace ToroChallenge.Application.AutoMapper.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserPositionModel, UserPositionResponse>();
+        }
+    }
+}
