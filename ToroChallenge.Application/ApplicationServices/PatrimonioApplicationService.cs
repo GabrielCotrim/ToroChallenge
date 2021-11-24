6ed0680bae1e3909901425c3dddd420405341ccb
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-24 11:32:30 -0300

    Adiciona application service

---
+++ ToroChallenge.Application/ApplicationServices/PatrimonioApplicationService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Application.DTOs.Responses;
+using ToroChallenge.Application.Interfaces.ApplicationServices;
+using ToroChallenge.Application.Interfaces.Services;
+
+namespace ToroChallenge.Application.ApplicationServices
+{
+    public class PatrimonioApplicationService : IPatrimonioApplicationService
+    {
+        private readonly IPatrimonioService _service;
+        private readonly IMapper _mapper;
+        public PatrimonioApplicationService(IPatrimonioService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public async Task<UserPositionResponse> ObtenhaPatrimonioDoUsuario(int idUser)
+        {
+            var patrimonio = await _service.ObtenhaPatrimonioDoUsuario(idUser);
+            return _mapper.Map<UserPositionResponse>(patrimonio);
+        }
+    }
+}
