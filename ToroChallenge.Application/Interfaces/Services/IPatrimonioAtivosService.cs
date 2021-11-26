35d68bed2db5d25fdf0e6fb873f0456193ed334d
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 16:17:51 -0300

    Adiciona serviço de patrimonio ativos

---
+++ ToroChallenge.Application/Interfaces/Services/IPatrimonioAtivosService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Domain.Entities;
+
+namespace ToroChallenge.Application.Interfaces.Services
+{
+    public interface IPatrimonioAtivosService
+    {
+        public Task<PatrimonioAtivos> CreateOrUpdatePatrimonioAtivos(PatrimonioAtivos patrimonioAtivos);
+    }
+}
---
+++ ToroChallenge.Application/Services/PatrimonioAtivosService.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using ToroChallenge.Application.Interfaces.Repositories;
+using ToroChallenge.Application.Interfaces.Services;
+using ToroChallenge.Application.Utils.Exceptions;
+using ToroChallenge.Domain.Entities;
+
+namespace ToroChallenge.Application.Services
+{
+    public class PatrimonioAtivosService : IPatrimonioAtivosService
+    {
+        private readonly IPatrimonioAtivosRepository _repository;
+        private readonly IAtivoRepository _ativoRepository;
+        private readonly IPatrimonioRepository _patrimonioRepository;
+        public PatrimonioAtivosService(IPatrimonioAtivosRepository repository, IAtivoRepository ativoRepository, IPatrimonioRepository patrimonioRepository)
+        {
+            _repository = repository;
+            _ativoRepository = ativoRepository;
+            _patrimonioRepository = patrimonioRepository;
+        }
+
+        public async Task<PatrimonioAtivos> CreateOrUpdatePatrimonioAtivos(PatrimonioAtivos patrimonioAtivos)
+        {
+            var patrimonio = await _patrimonioRepository.ObtenhaPatrimonioDoUsuario(patrimonioAtivos.Patrimonio.UsuarioId);
+            var ativo = await _ativoRepository.GetAtivo(patrimonioAtivos.Ativo.Symbol);
+
+            if (ativo == null) throw new NotFoundException();
+            if (patrimonio == null) throw new NotFoundException();
+
+            var patrimonioAtivosAtt = new PatrimonioAtivos { AtivoId = ativo.Id, PatrimonioId = patrimonio.Id, QuantidadeAtivos = patrimonioAtivos.QuantidadeAtivos };
+            var patrimonioAtivosRep = await _repository.GetPatrimonioAtivos(patrimonioAtivosAtt);
+
+            if (patrimonioAtivosRep == null) patrimonioAtivosRep = await _repository.CreatePatrimonioAtivos(patrimonioAtivosAtt);
+            else
+            {
+                patrimonioAtivosRep.QuantidadeAtivos = patrimonioAtivosRep.QuantidadeAtivos += patrimonioAtivos.QuantidadeAtivos;
+                patrimonioAtivosRep = await _repository.UpdatePatrimonioAtivos(patrimonioAtivosRep);
+            }
+
+            return patrimonioAtivosRep;
+        }
+    }
+}
