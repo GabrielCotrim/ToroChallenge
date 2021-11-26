4aa1b9d25fc165dc42e2d75724c04ac25e8dccd8
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 09:13:11 -0300

    Ajusta mapeamento para entidade dto

--- ToroChallenge.Application/AutoMapper/Mappings/MappingProfile.cs
+++ ToroChallenge.Application/AutoMapper/Mappings/MappingProfile.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ToroChallenge.Application.DTOs.Responses;
-using ToroChallenge.Domain.Models;
+using ToroChallenge.Domain.Entities;
 
 namespace ToroChallenge.Application.AutoMapper.Mappings
 {
@@ -13,7 +13,15 @@ namespace ToroChallenge.Application.AutoMapper.Mappings
     {
         public MappingProfile()
         {
-            CreateMap<UserPositionModel, UserPositionResponse>();
+            CreateMap<PatrimonioAtivos, PositionReponse>()
+                .ForMember(dest => dest.Amount, opt => opt.MapFrom(or => or.QuantidadeAtivos))
+                .ForMember(dest => dest.CurrentPrice, opt => opt.MapFrom(or => or.Ativo.CurrentPrice))
+                .ForMember(dest => dest.Symbol, opt => opt.MapFrom(or => or.Ativo.Symbol));
+
+            CreateMap<Patrimonio, UserPositionResponse>()
+                .ForMember(dest => dest.CheckingAccountAmount, opt => opt.MapFrom(or => or.Saldo))
+                .ForMember(dest => dest.Consolidated, opt => opt.MapFrom(or => or.Sumarizado))
+                .ForMember(dest => dest.Positions, opt => opt.MapFrom(or => or.PatrimonioAtivos));
         }
     }
 }
