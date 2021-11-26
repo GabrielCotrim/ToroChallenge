c806752b68808792986bbebfa56d7f7ef8ad9820
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 16:17:28 -0300

    Adiciona repositorio de patrimonio ativos

---
+++ ToroChallenge.Application/Interfaces/Repositories/IPatrimonioAtivosRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Domain.Entities;
+
+namespace ToroChallenge.Application.Interfaces.Repositories
+{
+    public interface IPatrimonioAtivosRepository
+    {
+        public Task<List<PatrimonioAtivos>> GetTopTrends(int quantidade);
+        public Task<PatrimonioAtivos> GetPatrimonioAtivos(PatrimonioAtivos patrimonioAtivos);
+        public Task<PatrimonioAtivos> CreatePatrimonioAtivos(PatrimonioAtivos patrimonioAtivos);
+        public Task<PatrimonioAtivos> UpdatePatrimonioAtivos(PatrimonioAtivos patrimonioAtivos);
+    }
+}
---
+++ ToroChallenge.Infrastructure/Repositories/PatrimonioAtivosRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Application.Interfaces.Repositories;
+using ToroChallenge.Application.Utils.Exceptions;
+using ToroChallenge.Domain.Entities;
+using ToroChallenge.Infrastructure.Data;
+
+namespace ToroChallenge.Infrastructure.Repositories
+{
+    public class PatrimonioAtivosRepository : IPatrimonioAtivosRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<PatrimonioAtivos> _dbSet;
+
+        public PatrimonioAtivosRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<PatrimonioAtivos>();
+        }
+
+        public Task<PatrimonioAtivos> CreatePatrimonioAtivos(PatrimonioAtivos patrimonioAtivos)
+        {
+            _dbSet.Add(patrimonioAtivos);
+            _context.SaveChanges();
+            return Task.FromResult(patrimonioAtivos);
+        }
+
+        public Task<PatrimonioAtivos> GetPatrimonioAtivos(PatrimonioAtivos patrimonioAtivos)
+        {
+            return _dbSet.FirstOrDefaultAsync(pa => pa.PatrimonioId == patrimonioAtivos.PatrimonioId && pa.AtivoId == patrimonioAtivos.AtivoId);
+        }
+
+        public Task<List<PatrimonioAtivos>> GetTopTrends(int quantidade)
+        {
+            var patrimonioAtivos = _dbSet
+                .Include(pa => pa.Ativo)
+                .OrderByDescending(pa => pa.QuantidadeAtivos)
+                .Take(quantidade)?.ToList();
+
+            return Task.FromResult(patrimonioAtivos);
+        }
+
+        public Task<PatrimonioAtivos> UpdatePatrimonioAtivos(PatrimonioAtivos patrimonioAtivos)
+        {
+            _context.Entry(patrimonioAtivos.Ativo).State = EntityState.Detached;
+            _context.Entry(patrimonioAtivos.Patrimonio).State = EntityState.Detached;
+            _dbSet.Update(patrimonioAtivos);
+            _context.SaveChanges();
+            return Task.FromResult(patrimonioAtivos);
+        }
+    }
+}
