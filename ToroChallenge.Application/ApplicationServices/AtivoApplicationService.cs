05d130d138e7a936081163a607667789a90dc27d
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 16:15:35 -0300

    Adiciona application service de ativos

---
+++ ToroChallenge.Application/ApplicationServices/AtivoApplicationService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Application.DTOs.Responses;
+using ToroChallenge.Application.Interfaces.ApplicationServices;
+using ToroChallenge.Application.Interfaces.Services;
+
+namespace ToroChallenge.Application.ApplicationServices
+{
+    public class AtivoApplicationService : IAtivoApplicationService
+    {
+        private readonly IAtivoService _service;
+        private readonly IMapper _mapper;
+        public AtivoApplicationService(IAtivoService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public async Task<List<TrendResponse>> GetTopTrends()
+        {
+            var topAtivos = await _service.GetTopAtivos();
+            return topAtivos.Select(ta => _mapper.Map<TrendResponse>(ta)).ToList();
+        }
+    }
+}
---
+++ ToroChallenge.Application/Interfaces/ApplicationServices/IAtivoApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Application.DTOs.Responses;
+
+namespace ToroChallenge.Application.Interfaces.ApplicationServices
+{
+    public interface IAtivoApplicationService
+    {
+        public Task<List<TrendResponse>> GetTopTrends();
+    }
+}
