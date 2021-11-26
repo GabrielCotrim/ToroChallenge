3f3c87b31fdf08d994ec84fc3ef45f003adbaaad
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 16:15:50 -0300

    Adiciona controller de ativo

---
+++ ToroChallenge.Api/Controllers/AtivoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToroChallenge.Application.DTOs.Responses;
+using ToroChallenge.Application.Interfaces.ApplicationServices;
+using ToroChallenge.Application.Utils.Exceptions;
+
+namespace ToroChallenge.Api.Controllers
+{
+    [Route("api/ativo")]
+    [ApiController]
+    public class AtivoController : ControllerBase
+    {
+        private readonly IAtivoApplicationService _applicationService;
+        public AtivoController(IAtivoApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        [HttpGet]
+        [Route("trends")]
+        [Produces("application/json")]
+        public async Task<ActionResult<IEnumerable<TrendResponse>>> GetTopTrends()
+        {
+            try
+            {
+                var trends = await _applicationService.GetTopTrends();
+                return Ok(trends);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
