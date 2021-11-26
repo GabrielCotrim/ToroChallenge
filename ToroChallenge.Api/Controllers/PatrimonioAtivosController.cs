65ce468df7bd0fc41bc8539a6692c2cadaf63d96
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 16:16:48 -0300

    Adiciona controller de patrimonio ativos

---
+++ ToroChallenge.Api/Controllers/PatrimonioAtivosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToroChallenge.Application.DTOs.Requests;
+using ToroChallenge.Application.DTOs.Responses;
+using ToroChallenge.Application.Interfaces.ApplicationServices;
+using ToroChallenge.Application.Utils.Exceptions;
+
+namespace ToroChallenge.Api.Controllers
+{
+    [Route("api/ativo")]
+    [ApiController]
+    public class PatrimonioAtivosController : ControllerBase
+    {
+        private readonly IPatrimonioAtivosApplicationService _applicationService;
+        public PatrimonioAtivosController(IPatrimonioAtivosApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        [HttpPost]
+        [Route("order")]
+        [Produces("application/json")]
+        public async Task<ActionResult<PositionReponse>> PostOrder([FromQuery] int id, [FromBody] OrderRequest order)
+        {
+            try
+            {
+                var orderSuccess = await _applicationService.CreateOrUpdatePatrimonioAtivos(id, order);
+                return Ok(orderSuccess);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
