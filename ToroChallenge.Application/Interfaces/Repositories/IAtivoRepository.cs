b540c8a3e7b8bdd010c6e8bc224251b264411c8d
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 16:15:13 -0300

    Adiciona repositorio de ativos

---
+++ ToroChallenge.Application/Interfaces/Repositories/IAtivoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Domain.Entities;
+
+namespace ToroChallenge.Application.Interfaces.Repositories
+{
+    public interface IAtivoRepository
+    {
+        public Task<List<Ativo>> GetAtivos();
+        public Task<Ativo> GetAtivo(string symbol);
+    }
+}
---
+++ ToroChallenge.Infrastructure/Repositories/AtivoRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Application.Interfaces.Repositories;
+using ToroChallenge.Domain.Entities;
+using ToroChallenge.Infrastructure.Data;
+
+namespace ToroChallenge.Infrastructure.Repositories
+{
+    public class AtivoRepository : IAtivoRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public AtivoRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<Ativo> GetAtivo(string symbol)
+        {
+            return _dbContext.Ativos.FirstOrDefaultAsync(a => a.Symbol == symbol);
+        }
+
+        public Task<List<Ativo>> GetAtivos()
+        {
+            return Task.FromResult(_dbContext.Ativos.ToList());
+        }
+    }
+}
