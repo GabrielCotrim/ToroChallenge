eac6ddbd86244068f5d35b81b36400402e4234f6
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 16:17:07 -0300

    Adiciona application service de patrimonio ativos

---
+++ ToroChallenge.Application/ApplicationServices/PatrimonioAtivosApplicationService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Application.DTOs.Requests;
+using ToroChallenge.Application.DTOs.Responses;
+using ToroChallenge.Application.Interfaces.ApplicationServices;
+using ToroChallenge.Application.Interfaces.Services;
+using ToroChallenge.Domain.Entities;
+
+namespace ToroChallenge.Application.ApplicationServices
+{
+    public class PatrimonioAtivosApplicationService : IPatrimonioAtivosApplicationService
+    {
+        private readonly IPatrimonioAtivosService _service;
+        private readonly IMapper _mapper;
+        public PatrimonioAtivosApplicationService(IPatrimonioAtivosService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public async Task<PositionReponse> CreateOrUpdatePatrimonioAtivos(int idUser, OrderRequest order)
+        {
+            var patrimonioAtivosRequest = _mapper.Map<PatrimonioAtivos>(order);
+            patrimonioAtivosRequest.Patrimonio = new Patrimonio { UsuarioId = idUser };
+            var patrimonioAtivos = await _service.CreateOrUpdatePatrimonioAtivos(patrimonioAtivosRequest);
+            return _mapper.Map<PositionReponse>(patrimonioAtivos);
+        }
+    }
+}
---
+++ ToroChallenge.Application/Interfaces/ApplicationServices/IPatrimonioAtivosApplicationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToroChallenge.Application.DTOs.Requests;
+using ToroChallenge.Application.DTOs.Responses;
+
+namespace ToroChallenge.Application.Interfaces.ApplicationServices
+{
+    public interface IPatrimonioAtivosApplicationService
+    {
+        public Task<PositionReponse> CreateOrUpdatePatrimonioAtivos(int idUser, OrderRequest order);
+    }
+}
