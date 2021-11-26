a39dd36a8c6b3791ed73b753cd21bac370f657e8
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 09:12:44 -0300

    Remove imports desnecessários

--- ToroChallenge.Application/ApplicationServices/PatrimonioApplicationService.cs
+++ ToroChallenge.Application/ApplicationServices/PatrimonioApplicationService.cs
@@ -1,8 +1,4 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using ToroChallenge.Application.DTOs.Responses;
 using ToroChallenge.Application.Interfaces.ApplicationServices;
