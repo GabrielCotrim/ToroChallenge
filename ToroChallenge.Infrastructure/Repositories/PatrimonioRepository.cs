6741ca583d8bf56d46039603c06191b7bec49979
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 09:17:13 -0300

    Adiciona db set ao repositório

--- ToroChallenge.Infrastructure/Repositories/PatrimonioRepository.cs
+++ ToroChallenge.Infrastructure/Repositories/PatrimonioRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,14 +14,18 @@ namespace ToroChallenge.Infrastructure.Repositories
     public sealed class PatrimonioRepository : IPatrimonioRepository
     {
         private readonly ApplicationDbContext _context;
+        private readonly DbSet<Patrimonio> _dbSet;
         public PatrimonioRepository(ApplicationDbContext context)
         {
             _context = context;
+            _dbSet = _context.Set<Patrimonio>();
         }
 
         public Task<Patrimonio> ObtenhaPatrimonioDoUsuario(int idUser)
         {
-            var patrimonio = _context.Patrimonios.FirstOrDefault(p => p.UsuarioId == idUser);
+            var patrimonio = _dbSet.Include(p => p.PatrimonioAtivos)
+                                .ThenInclude(pa => pa.Ativo)
+                                .FirstOrDefault(p => p.UsuarioId == idUser);
             if (patrimonio == null) throw new NotFoundException();
             return Task.FromResult(patrimonio);
         }
